cc0ec0f864199319ff0b6050607fe7847dcfb202
Mythical-Github <MythicalData@gmail.com>
2023-06-13 23:14:12 -0400

    Fixed Annoying Steam Prompt When Testing
    Fixed Annoying Steam Prompt When Testing

--- Src/Mythical/Classes/KF_Mutator_Mythical.uc
+++ Src/Mythical/Classes/KF_Mutator_Mythical.uc
@@ -3,6 +3,10 @@ class KF_Mutator_Mythical extends KFMutator;
 * This function can be used to parse the command line parameters when a server
 * starts up
 */
+
+var bool bDoOnce;
+
+
 function InitMutator(string Options, out string ErrorMessage)
 {
 	local Actor A;
@@ -23,3 +27,18 @@ event PostBeginPlay()
 {
 	`log("PostBeginPlay Was Called");
 }
+
+event Tick(float DeltaTime)
+{
+	DoOnceOnTickExample();
+}
+
+
+function DoOnceOnTickExample()
+{
+	if( bDoOnce == False )
+	{
+		`log("Do Once On Tick Example Has Fired");
+		bDoOnce = True;
+	}
+} 
--- scripts/python/test_unpublished_mods.py
+++ scripts/python/test_unpublished_mods.py
@@ -1,15 +1,15 @@
-import os
+import subprocess
 
 
-package_name = r"Mythical"
-game_mode = r"KFGameInfo_Mythical"
-map_name = r"KF-ZedsDiner"
-mutator_name = r"KF_Mutator_Mythical"
-kf2_dir = r"C:\Program Files (x86)\Steam\steamapps\common\killingfloor2\Binaries\Win64"
+steam_exe = "C:\Program Files (x86)\Steam\steam.exe"
+package_name = "Mythical"
+game_mode = "KFGameInfo_Mythical"
+map_name = "KF-ZedsDiner"
+mutator_name = "KF_Mutator_Mythical"
+steam_app_id = 232090
 
 
-os.chdir(kf2_dir)
-os.system(f"KFGame.exe {map_name}?game={package_name}.{game_mode}?mutator={package_name}.{mutator_name} -log -useunpublished")
+subprocess.run(f"{steam_exe} -applaunch {steam_app_id} {map_name}?game={package_name}.{game_mode}?mutator={package_name}.{mutator_name} -log -useunpublished")
 
 
 quit()

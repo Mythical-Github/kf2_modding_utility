8379607a30c5d47557c100e85ea6d596eb00c732
Mythical-Github <MythicalData@gmail.com>
2023-06-16 18:10:34 -0400

    Actually Added Py Files This Time
    Actually Added Py Files This Time

---
+++ scripts/python/easy_server_testing/kf2_client_testing.py
@@ -0,0 +1,32 @@
+import os
+import socket
+import subprocess
+
+
+workshup_uploader_py = r"C:\Users\Mythical\Documents\GitHub\kf2_mythical\scripts\python\upload_mod_to_workshop_alt_method\upload_mod_to_workshop_alt_method.py"
+open_kf2_to_server_py = r"C:\Users\Mythical\Documents\GitHub\kf2_mythical\scripts\python\open_kf2_to_server.py"
+
+# Host and port of the VPS
+host = ""
+port = 
+
+subprocess.run(["python", workshup_uploader_py])
+
+# Command to send to the VPS
+command_to_send = 'run_command'
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect((host, port))
+
+# Send the command to the server
+client_socket.sendall(command_to_send.encode())
+
+# Close the connection
+client_socket.close()
+
+os.system("taskkill /f /im steam.exe")
+
+subprocess.Popen(["python", open_kf2_to_server_py])

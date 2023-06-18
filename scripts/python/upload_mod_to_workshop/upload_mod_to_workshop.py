414f7c45afeb184d3b1137fde98aa63aa117abee
Mythical-Github <MythicalData@gmail.com>
2023-06-17 22:55:22 -0400

    Updated Upload To Workshop Py To Run From settings.json
    Updated Upload To Workshop Py To Run From settings.json

--- scripts/python/upload_mod_to_workshop/upload_mod_to_workshop.py
+++ scripts/python/upload_mod_to_workshop/upload_mod_to_workshop.py
@@ -1,7 +1,16 @@
+import json
 import subprocess
 
-workshop_user_tool_exe = r""
-upload_info_text = r""
+settings_json = r"C:\Users\Mythical\Documents\GitHub\kf2_mythical\other\settings.json"
+
+with open(settings_json) as file:
+    data = json.load(file)
+
+part_a = data["kf2_game_dir"]
+part_b = "Binaries\WorkshopUserTool.exe"
+
+workshop_user_tool_exe = f"{part_a}\{part_b}"
+upload_info_text = data["upload_info_text"]
 
 subprocess.run([workshop_user_tool_exe, upload_info_text])
 

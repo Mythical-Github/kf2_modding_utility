0d9ef3d1d03403541c81166f0f8643b3952548c0
Mythical-Github <MythicalData@gmail.com>
2023-06-18 11:10:37 -0400

    Changed Default Open Directory For Umodel
    Changed Default Open Directory For Umodel

--- scripts/python/open_umodel.py
+++ scripts/python/open_umodel.py
@@ -1,3 +1,4 @@
+import os
 import json
 import subprocess
 
@@ -7,6 +8,10 @@ with open(settings_json) as file:
     data = json.load(file)
 
 umodel_exe = data["umodel_exe"]
+
+kf2_dir = data["kf2_game_dir"]
+
+os.chdir(kf2_dir)
 
 subprocess.run([umodel_exe])
 

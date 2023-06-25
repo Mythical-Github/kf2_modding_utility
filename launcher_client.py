9fb431963637a156247608a5783a2f4243d9d392
Mythical-Github <MythicalData@gmail.com>
2023-06-25 17:42:08 -0400

    Added Launcher Scripts For Organization
    Added Launcher Scripts For Organization

---
+++ launcher_client.py
@@ -0,0 +1,10 @@
+import sys
+from kf2_modding_utility.kf2_modding_utility_client import kf2_modding_utility_client
+
+
+def main():
+    kf2_modding_utility_client.main()
+
+
+if __name__ == "__main__":
+    main()

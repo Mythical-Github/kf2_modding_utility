b1801b82994268cb1236137dff70ccaa52a07698
Mythical-Github <MythicalData@gmail.com>
2023-07-10 20:18:16 -0400

    Code Cleanup
    Code Cleanup

--- kf2_modding_utility/kf2_modding_utility_client/scripts/python/open_webadmin.py
+++ kf2_modding_utility/kf2_modding_utility_client/scripts/python/open_webadmin.py
@@ -1,11 +1,13 @@
+import sys
 import json
 import webbrowser
 from pathlib import Path
+from main import SETTINGS_JSON
 
-settings_json = Path(__file__).resolve().parent.parent.parent / 'settings' / 'settings.json'
 
-with open(settings_json) as file:
+with open(SETTINGS_JSON) as file:
     data = json.load(file)
+
 
 part_a = data["kf2_server_ip"]
 part_b = data["webadmin_port"]
@@ -15,3 +17,6 @@ edge_path = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
 # using edge because wedadmin doesn't like firefox for me for whatever reason
 webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
 webbrowser.get('edge').open(url)
+
+
+sys.exit()

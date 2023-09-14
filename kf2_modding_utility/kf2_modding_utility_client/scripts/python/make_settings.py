4da0fd81097435101cb9a13715fb7ef5f0b0bd85
Mythical-Github <MythicalData@gmail.com>
2023-09-14 03:50:16 -0400

    Fixed Launch Scripts
    Fixed Launch Scripts

--- kf2_modding_utility/kf2_modding_utility_client/scripts/python/make_settings.py
+++ kf2_modding_utility/kf2_modding_utility_client/scripts/python/make_settings.py
@@ -1,6 +1,6 @@
 import sys
+import json
 from main import SETTINGS_JSON
-
 
 SETTINGS = {
     "kf2_server_ip": "127.0.0.1",
@@ -11,17 +11,17 @@ SETTINGS = {
     "easy_testing_port": 7780,
     "webadmin_port": 7779,
     "game_port": 7777,
-    "vps_access_url": "https://remotedesktop.google.com/access/",
-    "steam_exe": r"C:\\Program Files (x86)\\Steam\\steam.exe",
-    "steam_cmd_exe": r"C:\\Programs\\steamcmd\\steamcmd.exe",
-    "umodel_exe": r"C:\\Modding\\Programs\\Umodel\\umodel_64.exe",
-    "blender_exe": r"C:\\Program Files\\Blender Foundation\\Blender 3.5\\blender.exe",
-    "kf2_game_dir": r"C:\\Program Files (x86)\\Steam\\steamapps\\common\\killingfloor2",
-    "kf2_docs_dir": r"C:\\Users\\Mythical\\Documents\\My Games\\KillingFloor2",
+    "vps_access_url": "https:/remotedesktop.google.com/access/",
+    "steam_exe": "C:/Program Files (x86)/Steam/steam.exe",
+    "steam_cmd_exe": "C:/Programs/steamcmd/steamcmd.exe",
+    "umodel_exe": "C:/Modding/Programs/Umodel/umodel_64.exe",
+    "blender_exe": "C:/Program Files/Blender Foundation/Blender 3.5/blender.exe",
+    "kf2_game_dir": "C:/Program Files (x86)/Steam/steamapps/common/killingfloor2",
+    "kf2_docs_dir": "C:/Users/Mythical/Documents/My Games/KillingFloor2",
     "steam_username": "fake_username",
     "steam_password": "fake_password",
-    "kf_editor_ini": r"C:\\Users\\Mythical\\Documents\\My Games\\KillingFloor2\\KFGame\\Config\\KFEditor.ini",
-    "ide_exe": "C:\\Users\\Mythical\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe",
+    "kf_editor_ini": "C:/Users/Mythical/Documents/My Games/KillingFloor2/KFGame/Config/KFEditor.ini",
+    "ide_exe": "C:/Users/Mythical/AppData/Local/Programs/Microsoft VS Code/Code.exe",
     "publishedfileid": "0",
     "contentfolder": "C:/Users/Mythical/Documents/GitHub/kf2_mythical/kf2_modding_utility/scripts/python/upload_mod_to_workshop_alt_method/mod_upload_dir",
     "previewfile": "C:/Users/Mythical/Documents/GitHub/kf2_mythical/kf2_modding_utility/scripts/python/upload_mod_to_workshop_alt_method/Testing.png",
@@ -32,10 +32,7 @@ SETTINGS = {
     "show_log_window": "false"
 }
 
-
-with open(SETTINGS_JSON, 'w') as json:
-    json.dump(SETTINGS, json, indent=4)
-
+with open(SETTINGS_JSON, 'w') as json_file:
+    json.dump(SETTINGS, json_file, indent=4)  # Use json.dump, not json_file.dump
 
 sys.exit()
-
--- kf2_modding_utility/launch_server.py
+++ kf2_modding_utility/launch_server.py
@@ -1,24 +1,12 @@
 import os
-import json
+import sys
 import subprocess
 
-
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-SETTINGS_JSON = os.path.normpath(os.path.join(SCRIPT_DIR, "kf2_modding_utility_client/settings/settings.json"))
+NEW_SCRIPT_DIR = f"{SCRIPT_DIR}/kf2_modding_utility_server/scripts/python"
 
+os.chdir(NEW_SCRIPT_DIR)
 
-with open(SETTINGS_JSON) as settings_json:
-    settings_data = json.load(settings_json)
+subprocess.Popen("python main.py")
 
-script_path = os.path.join(SCRIPT_DIR, "kf2_modding_utility_server/scripts/python/main.py")
-
-
-def main():
-    if settings_data["show_log_window"] == "true":
-        subprocess.Popen(["python", script_path])
-    else:
-        subprocess.Popen(["pythonw", script_path])
-
-
-if __name__ == "__main__":
-    main()
+sys.exit()
